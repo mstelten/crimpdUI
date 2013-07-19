77bf628b467d64309e0d14b9638534f76654b4cd
Mike Stelten <mike.stelten@orbitz.com>
2013-07-19 11:29:49 -0500

    changing metadata names to lowercase

--- public/js/exercise/search.ctrl.js
+++ public/js/exercise/search.ctrl.js
@@ -12,10 +12,10 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 	$scope.search = exerciseData.getCachedSearch();
 	$scope.clearFilters = function () {
 		var clean = {
-			Target: "",
-			Difficulty: "",
-			Type: "",
-			Equipment: "",
+			target: "",
+			difficulty: "",
+			type: "",
+			equipment: "",
 			$: ""
 		};
 		$scope.search = clean;
@@ -24,9 +24,9 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 	$scope.targetOptions = function () {
 		var targets = [];
 		for (var i = 0; i < $scope.exercises.length; i++) {
-			if ((!$scope.search.Difficulty || _.contains($scope.exercises[i].Difficulty, $scope.search.Difficulty))
-			&& (!$scope.search.Type || _.contains($scope.exercises[i].Type, $scope.search.Type))
-			&& (!$scope.search.Equipment || _.contains($scope.exercises[i].Equipment, $scope.search.Equipment))
+			if ((!$scope.search.difficulty || _.contains($scope.exercises[i].difficulty, $scope.search.difficulty))
+			&& (!$scope.search.type || _.contains($scope.exercises[i].type, $scope.search.type))
+			&& (!$scope.search.equipment || _.contains($scope.exercises[i].equipment, $scope.search.equipment))
 			&& (!$scope.search.$ || _.some(_.values($scope.exercises[i]), function(z) {
 				if (_.isString(z)) {
 					if (z.toLowerCase().indexOf($scope.search.$.toLowerCase()) > -1) return true;
@@ -36,10 +36,10 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 					});
 				}
 			}))) {
-				if ($scope.exercises[i].Target != undefined) {
-					for (var j = 0; j < $scope.exercises[i].Target.length; j++) {
-						if (targets.indexOf($scope.exercises[i].Target[j]) == -1)
-							targets.push($scope.exercises[i].Target[j]);
+				if ($scope.exercises[i].target != undefined) {
+					for (var j = 0; j < $scope.exercises[i].target.length; j++) {
+						if (targets.indexOf($scope.exercises[i].target[j]) == -1)
+							targets.push($scope.exercises[i].target[j]);
 					}
 				}
 			}
@@ -49,9 +49,9 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 	$scope.difficultyOptions = function () {
 		var difficulties = [];
 		for (var i = 0; i < $scope.exercises.length; i++) {
-			if ((!$scope.search.Target || _.contains($scope.exercises[i].Target, $scope.search.Target))
-			&& (!$scope.search.Type || _.contains($scope.exercises[i].Type, $scope.search.Type))
-			&& (!$scope.search.Equipment || _.contains($scope.exercises[i].Equipment, $scope.search.Equipment))
+			if ((!$scope.search.target || _.contains($scope.exercises[i].target, $scope.search.target))
+			&& (!$scope.search.type || _.contains($scope.exercises[i].type, $scope.search.type))
+			&& (!$scope.search.equipment || _.contains($scope.exercises[i].equipment, $scope.search.equipment))
 			&& (!$scope.search.$ || _.some(_.values($scope.exercises[i]), function (z) {
 				if (_.isString(z)) {
 					if (z.toLowerCase().indexOf($scope.search.$.toLowerCase()) > -1) return true;
@@ -61,10 +61,10 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 					});
 				}
 			}))) {
-				if ($scope.exercises[i].Difficulty != undefined) {
-					for (var j = 0; j < $scope.exercises[i].Difficulty.length; j++) {
-						if (difficulties.indexOf($scope.exercises[i].Difficulty[j]) == -1)
-							difficulties.push($scope.exercises[i].Difficulty[j]);
+				if ($scope.exercises[i].difficulty != undefined) {
+					for (var j = 0; j < $scope.exercises[i].difficulty.length; j++) {
+						if (difficulties.indexOf($scope.exercises[i].difficulty[j]) == -1)
+							difficulties.push($scope.exercises[i].difficulty[j]);
 					}
 				}
 			}
@@ -74,9 +74,9 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 	$scope.typeOptions = function () {
 		var types = [];
 		for (var i = 0; i < $scope.exercises.length; i++) {
-			if ((!$scope.search.Target || _.contains($scope.exercises[i].Target, $scope.search.Target))
-			&& (!$scope.search.Difficulty || _.contains($scope.exercises[i].Difficulty, $scope.search.Difficulty))
-			&& (!$scope.search.Equipment || _.contains($scope.exercises[i].Equipment, $scope.search.Equipment))
+			if ((!$scope.search.target || _.contains($scope.exercises[i].target, $scope.search.target))
+			&& (!$scope.search.difficulty || _.contains($scope.exercises[i].difficulty, $scope.search.difficulty))
+			&& (!$scope.search.equipment || _.contains($scope.exercises[i].equipment, $scope.search.equipment))
 			&& (!$scope.search.$ || _.some(_.values($scope.exercises[i]), function (z) {
 				if (_.isString(z)) {
 					if (z.toLowerCase().indexOf($scope.search.$.toLowerCase()) > -1) return true;
@@ -86,10 +86,10 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 					});
 				}
 			}))) {
-				if ($scope.exercises[i].Type != undefined) {
-					for (var j = 0; j < $scope.exercises[i].Type.length; j++) {
-						if (types.indexOf($scope.exercises[i].Type[j]) == -1)
-							types.push($scope.exercises[i].Type[j]);
+				if ($scope.exercises[i].type != undefined) {
+					for (var j = 0; j < $scope.exercises[i].type.length; j++) {
+						if (types.indexOf($scope.exercises[i].type[j]) == -1)
+							types.push($scope.exercises[i].type[j]);
 					}
 				}
 			}
@@ -99,9 +99,9 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 	$scope.equipmentOptions = function () {
 		var equipments = [];
 		for (var i = 0; i < $scope.exercises.length; i++) {
-			if ((!$scope.search.Target || _.contains($scope.exercises[i].Target, $scope.search.Target))
-			&& (!$scope.search.Difficulty || _.contains($scope.exercises[i].Difficulty, $scope.search.Difficulty))
-			&& (!$scope.search.Type || _.contains($scope.exercises[i].Type, $scope.search.Type))
+			if ((!$scope.search.target || _.contains($scope.exercises[i].target, $scope.search.target))
+			&& (!$scope.search.difficulty || _.contains($scope.exercises[i].difficulty, $scope.search.difficulty))
+			&& (!$scope.search.type || _.contains($scope.exercises[i].type, $scope.search.type))
 			&& (!$scope.search.$ || _.some(_.values($scope.exercises[i]), function (z) {
 				if (_.isString(z)) {
 					if (z.toLowerCase().indexOf($scope.search.$.toLowerCase()) > -1) return true;
@@ -111,10 +111,10 @@ function ExerciseSearchCtrl($scope, exerciseData) {
 					});
 				}
 			}))) {
-				if ($scope.exercises[i].Equipment != undefined) {
-					for (var j = 0; j < $scope.exercises[i].Equipment.length; j++) {
-						if (equipments.indexOf($scope.exercises[i].Equipment[j]) == -1)
-							equipments.push($scope.exercises[i].Equipment[j]);
+				if ($scope.exercises[i].equipment != undefined) {
+					for (var j = 0; j < $scope.exercises[i].equipment.length; j++) {
+						if (equipments.indexOf($scope.exercises[i].equipment[j]) == -1)
+							equipments.push($scope.exercises[i].equipment[j]);
 					}
 				}
 			}
