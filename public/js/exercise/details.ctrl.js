f9300c7d145034d2c240450835133f3f8d318b72
Mike Stelten <mstelten@gmail.com>
2013-07-24 00:22:31 -0400

    exercise details image adjustment

--- public/js/exercise/details.ctrl.js
+++ public/js/exercise/details.ctrl.js
@@ -3,7 +3,6 @@ function ExerciseDetailsCtrl($scope, $routeParams, exerciseData) {
 	exerciseData.querySingleExercise($routeParams.exerciseId).then(function (data) {
 		$scope.exr = data;
 	});
-	$scope.imgBaseUrl = "http://aa59356d6677171dbd8d-142fcf5163b4da6936f69137fa2786a7.r87.cf2.rackcdn.com";
 	$scope.equipment = function () {
 		var equips = [];
 		for (var i=0; i < $scope.exr.metadata.length; i++ ) {
